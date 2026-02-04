7302686450306eba4cd16aa352f57e8216988e07
Nickson1301 <rojarnicksonrojarnickson@gmail.com>
2026-02-04 17:28:59 +0530

    add the images and created image constant file

--- src/components/ChildComp.js
+++ src/components/ChildComp.js
@@ -1,14 +1,21 @@
 import { Component } from "react";
 import "./external.css"
+import constdata from "../shared/constant/constantfile";
 class ChildComp extends Component {
     render() {
-        const{name,salary}=this.props
+        const { name, salary } = this.props
         return (
             <>
                 <h1 >child components</h1>
                 <div className="txt">name: {name}</div>
                 <div className="txt1">salary: {salary}</div>
-                <button className="but1" onClick={()=>this.props.update()}>increment</button>
+                <button className="but1" onClick={() => this.props.update()}>increment</button>
+                <img src={constdata.fish1} alt="img" className="imgs"></img>
+                <img src={constdata.goldfish} alt="img" className="imgs"></img>
+                <img src={constdata.fighter} alt="img" className="imgs"></img>
+                <img src={constdata.fish2} alt="img" className="imgs"></img>
+
+
 
             </>
         )
---
+++ src/shared/constant/constantfile.js
@@ -0,0 +1,10 @@
+import fish1 from '../images/fish1.jpg';
+import goldfish from '../images/goldfish.png'
+import fighter from '../images/fighter.jfif'
+import fish2 from '../images/fish2.jfif';
+
+const constdata={
+    fish1,goldfish,fighter,fish2
+}
+
+export default constdata;
