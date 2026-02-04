1b85e5d04a8f0211b842075b2fd27d6393db08a2
Nickson1301 <rojarnicksonrojarnickson@gmail.com>
2026-02-04 16:34:17 +0530

    add externalcss and parent child class

--- src/components/ChildComp.js
+++ src/components/ChildComp.js
@@ -1,14 +1,14 @@
 import { Component } from "react";
-
+import "./external.css"
 class ChildComp extends Component {
     render() {
         const{name,salary}=this.props
         return (
             <>
-                <h1>child components</h1>
-                <div>name: {name}</div>
-                <div>salary: {salary}</div>
-                <button onClick={()=>this.props.update()}>increment</button>
+                <h1 >child components</h1>
+                <div className="txt">name: {name}</div>
+                <div className="txt1">salary: {salary}</div>
+                <button className="but1" onClick={()=>this.props.update()}>increment</button>
 
             </>
         )
--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import ChildComp from "./ChildComp";
 import MyPureComp from "./MyPureComp";
 import MyReactMemoComp from "./MyReactMemoComp";
-
+import "./external.css"
 
 class ParentComp extends Component{
 
@@ -22,9 +22,9 @@ class ParentComp extends Component{
         console.log("parent component")
         return(
             <>
-             <div>Employee name : <strong>{empname}</strong></div>
-             <div>Employee name : <strong>{empsalary}</strong></div>
-             <button onClick={()=>this.updatesal()}>increment</button>
+             <div className="txt">Employee name : <strong>{empname}</strong></div>
+             <div className="txt1">Employee name : <strong>{empsalary}</strong></div>
+             <button onClick={()=>this.updatesal()} className="but1">increment</button>
              <ChildComp name={empname} salary={empsalary} update={this.updatesal}/>
              <MyPureComp ename={empname}/>
              <MyReactMemoComp/>
